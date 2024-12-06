df9a35fed7070b5a01622a53e81896f5a5321a05
Chetan Ganji <monarch@dragonsden.black>
2024-12-06 14:06:09 +0530

    staging: Updates book detail page

--- pages/books/[id].vue
+++ pages/books/[id].vue
@@ -1,13 +1,36 @@
 <template>
+    
     <div>
         <h1> Book Detail Page</h1>
-        <h1> Book ID {{ id }}</h1>
+        <h1> Book ID {{ book_id }}</h1>
     </div>
+
+    <div v-if="book">
+        <h2>Title: {{ book.title }}</h2>
+        <h2>Description: {{ book.description }}</h2>
+        <!-- <h2>Author : {{ book.author }}</h2> -->
+        <h2>Recommended : {{ book.is_recommended }}</h2>
+        <h2>Average Rating : {{ book.avg_rating }}</h2>
+    </div>
+
 </template>
 
 <script setup>
+
+const runtimeConfig = useRuntimeConfig()
 const route = useRoute()
-const id = route.params.id
+const book_id = route.params.id
+const currentUrl = runtimeConfig.public.apiBase + "books/" + book_id
+
+// console.log(runtimeConfig.public.apiBase)
+// console.log(currentUrl)
+let book = false;
+
+const { data } = await useFetch(currentUrl)
+
+book = data.value;
+
 console.log(route.params.id)
+console.log(data.value);
 
 </script>
